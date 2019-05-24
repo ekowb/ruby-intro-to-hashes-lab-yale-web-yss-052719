24bbad4c31514a2b402338613ef7620a1768c144
Ekow Buadu <ekow.buadu@yale.edu>
2019-05-24 14:44:06 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,6 +16,13 @@ def monopoly
         :two_pieces_owned => 50,
         :three_pieces_owned => 100,
         :four_pieces_owned => 200
+      },
+      :names =>
+      {
+        :reading_railroad =>
+        {
+          "mortgage_value" => "$200"
+        }
       }
 
   }
