232f257246ddd091240a68818293c449d557dfdb
Ekow Buadu <ekow.buadu@yale.edu>
2019-05-24 14:58:45 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,8 @@ def monopoly_with_second_tier
   monopoly = {
     :railroads => {
       :pieces=> 4,
-        :rent_in_dollars => {}
+      :rent_in_dollars => {}
+      :names => {}
     }
   }
 
@@ -28,14 +29,14 @@ end
 def monopoly_with_third_tier
   monopoly = {
     :railroads => {
-      :pieces=> 4, {
-        :rent_in_dollars => {
+      :pieces=> 4,
+      :rent_in_dollars => {
           :one_piece_owned => 25,
           :two_pieces_owned => 50,
           :three_pieces_owned => 100,
           :four_pieces_owned => 200
         },
-          :names =>
+        :names =>
           {
             :reading_railroad=> {}
             :pennsylvania_railroad=> {}
